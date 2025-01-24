3cbe63363cc8a35064ab53ba198d5a5adc5758ae
AzerRom <150341448+AzerRom@users.noreply.github.com>
2025-01-24 05:25:59 +0100

    db connection started

---
+++ growth_backend/scr/config.php
@@ -0,0 +1,7 @@
+ECHO ist eingeschaltet (ON).
+<?php 
+# Autoloader von Composer
+require_once 'C:\Users\SMO450\dev\xampp\phpMyAdmin\vendor\autoload.php'; 
+
+#Für die Umgebungsvariabelen aus .env
+$
